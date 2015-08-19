5ef8c437a73a8e7489e1f73c0bbeebd6d718a8a9
lynnfield <genovich.v.v@gmail.com>
2015-08-19 12:42:42 +0300

    add TaskStateChangeList

---
+++ app/src/main/java/com/gensko/projectmanager/models/TaskStateChangeList.java
@@ -0,0 +1,11 @@
+package com.gensko.projectmanager.models;
+
+import com.gensko.projectmanager.models.domain.TaskStateChange;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Genovich V.V. on 19.08.2015.
+ */
+@SuppressWarnings("DefaultFileTemplate")
+public class TaskStateChangeList extends ArrayList<TaskStateChange> {}
